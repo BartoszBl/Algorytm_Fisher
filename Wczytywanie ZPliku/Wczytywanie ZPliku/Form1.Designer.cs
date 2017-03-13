488b594bb93cda8238fb6cdac66c43ead79b5e4b
BartoszBlicharski <bartek.blicharski@gmail.com>
2017-03-13 12:55:43 +0100

    Algorytm Fishera

---
+++ Wczytywanie ZPliku/Wczytywanie ZPliku/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace Wczytywanie_ZPliku
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.WybierzSystem = new System.Windows.Forms.Button();
+            this.tbSciezka = new System.Windows.Forms.TextBox();
+            this.ofd = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.WczytanySystem = new System.Windows.Forms.RichTextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.KoncowaSelekcja = new System.Windows.Forms.RichTextBox();
+            this.Generuj = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.IloscAtrybutow = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.wyczysc = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.IloscAtrybutow)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // WybierzSystem
+            // 
+            this.WybierzSystem.Location = new System.Drawing.Point(13, 39);
+            this.WybierzSystem.Margin = new System.Windows.Forms.Padding(2);
+            this.WybierzSystem.Name = "WybierzSystem";
+            this.WybierzSystem.Size = new System.Drawing.Size(83, 53);
+            this.WybierzSystem.TabIndex = 0;
+            this.WybierzSystem.Text = "Wczytaj System Decyzyjny";
+            this.WybierzSystem.UseVisualStyleBackColor = true;
+            this.WybierzSystem.Click += new System.EventHandler(this.WybierzSystem_Click);
+            // 
+            // tbSciezka
+            // 
+            this.tbSciezka.Enabled = false;
+            this.tbSciezka.Location = new System.Drawing.Point(110, 39);
+            this.tbSciezka.Margin = new System.Windows.Forms.Padding(2);
+            this.tbSciezka.Name = "tbSciezka";
+            this.tbSciezka.ReadOnly = true;
+            this.tbSciezka.Size = new System.Drawing.Size(294, 20);
+            this.tbSciezka.TabIndex = 1;
+            // 
+            // ofd
+            // 
+            this.ofd.FileName = "openFileDialog1";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.WczytanySystem);
+            this.groupBox1.Location = new System.Drawing.Point(147, 65);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(145, 148);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Wczytany System decyzyjny:";
+            // 
+            // WczytanySystem
+            // 
+            this.WczytanySystem.Enabled = false;
+            this.WczytanySystem.Location = new System.Drawing.Point(6, 39);
+            this.WczytanySystem.Name = "WczytanySystem";
+            this.WczytanySystem.Size = new System.Drawing.Size(133, 109);
+            this.WczytanySystem.TabIndex = 0;
+            this.WczytanySystem.Text = "";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.KoncowaSelekcja);
+            this.groupBox2.Location = new System.Drawing.Point(147, 251);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(151, 182);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "System decyzyjny po selekcji cech:";
+            // 
+            // KoncowaSelekcja
+            // 
+            this.KoncowaSelekcja.Enabled = false;
+            this.KoncowaSelekcja.Location = new System.Drawing.Point(24, 34);
+            this.KoncowaSelekcja.Name = "KoncowaSelekcja";
+            this.KoncowaSelekcja.Size = new System.Drawing.Size(121, 121);
+            this.KoncowaSelekcja.TabIndex = 1;
+            this.KoncowaSelekcja.Text = "";
+            this.KoncowaSelekcja.TextChanged += new System.EventHandler(this.KoncowaSelekcja_TextChanged);
+            // 
+            // Generuj
+            // 
+            this.Generuj.AllowDrop = true;
+            this.Generuj.Location = new System.Drawing.Point(289, 219);
+            this.Generuj.Name = "Generuj";
+            this.Generuj.Size = new System.Drawing.Size(78, 26);
+            this.Generuj.TabIndex = 4;
+            this.Generuj.Text = "Generuj";
+            this.Generuj.UseVisualStyleBackColor = true;
+            this.Generuj.Click += new System.EventHandler(this.Generuj_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(329, 136);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // IloscAtrybutow
+            // 
+            this.IloscAtrybutow.Location = new System.Drawing.Point(147, 219);
+            this.IloscAtrybutow.Name = "IloscAtrybutow";
+            this.IloscAtrybutow.Size = new System.Drawing.Size(78, 20);
+            this.IloscAtrybutow.TabIndex = 5;
+            this.IloscAtrybutow.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.IloscAtrybutow.ValueChanged += new System.EventHandler(this.IloscAtrybutow_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(10, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(404, 16);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Selekcja atrybutów systemu decyzyjnego metodą Fishera";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 221);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(128, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Wybierz Liczbę atrybutów";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Enabled = false;
+            this.richTextBox1.Location = new System.Drawing.Point(22, 294);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(100, 112);
+            this.richTextBox1.TabIndex = 10;
+            this.richTextBox1.Text = "Pamiętaj!          Liczba wybranych atrybutów nie może być większa od wczytanej l" +
+    "iczby atrybutów";
+            // 
+            // wyczysc
+            // 
+            this.wyczysc.BackColor = System.Drawing.Color.Yellow;
+            this.wyczysc.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.wyczysc.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.wyczysc.Location = new System.Drawing.Point(304, 383);
+            this.wyczysc.Name = "wyczysc";
+            this.wyczysc.Size = new System.Drawing.Size(75, 23);
+            this.wyczysc.TabIndex = 11;
+            this.wyczysc.Text = "Wyczyść";
+            this.wyczysc.UseVisualStyleBackColor = false;
+            this.wyczysc.Click += new System.EventHandler(this.wyczysc_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.Location = new System.Drawing.Point(304, 410);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Zamknij";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.zamknij_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(426, 452);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.wyczysc);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.IloscAtrybutow);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Generuj);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.tbSciezka);
+            this.Controls.Add(this.WybierzSystem);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Selekcja atrybutów metodą Fishera";
+            this.Load += new System.EventHandler(this.WczytanySystem_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.IloscAtrybutow)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button WybierzSystem;
+        private System.Windows.Forms.TextBox tbSciezka;
+        private System.Windows.Forms.OpenFileDialog ofd;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button Generuj;
+        private System.Windows.Forms.RichTextBox WczytanySystem;
+        private System.Windows.Forms.RichTextBox KoncowaSelekcja;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown IloscAtrybutow;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button wyczysc;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Wczytywanie ZPliku/Wczytywanie ZPliku/Form1.cs
@@ -0,0 +1,338 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Wczytywanie_ZPliku
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        int[][] daneZPliku;
+
+
+        int[] WartosciDecyzji()
+        {
+            List<int> decyzje = new List<int>();
+            int decyzja;
+
+
+           
+            for (int i = 0; i < daneZPliku.Length; i ++)
+            {
+                decyzja = daneZPliku[i].Last();
+
+                decyzje.Add(decyzja);
+            }
+
+            decyzje = decyzje.Distinct().ToList();
+
+            return decyzje.ToArray(); 
+        }
+
+        int[] WartosciZKlasy(int[][] tab, int decyzja, int indeksAtrybutu)
+        {
+            List<int> lista = new List<int>();
+
+            for (int i = 0; i < tab.Length; i++)
+            {
+                if (tab[i].Last() == decyzja)
+                {
+                    lista.Add(tab[i][indeksAtrybutu]);
+                }
+            }
+
+            return lista.ToArray();
+        }
+
+        int[] WartosciPozostale(int[][] tab, int decyzja, int indeksAtrybutu)
+        {
+            List<int> lista = new List<int>();
+
+            for (int i = 0; i < tab.Length; i++)
+            {
+                if (tab[i].Last() != decyzja)
+                {
+                    lista.Add(tab[i][indeksAtrybutu]);
+                }
+            }
+
+            return lista.ToArray();
+        }
+
+
+       
+        double Srednia (int [] tab)
+        {
+            double suma = 0;
+            double srednia = 0;
+            for (int i = 0; i < tab.Length; i++)
+            {
+                suma = suma + tab[i];
+            }
+
+
+            srednia = suma / tab.Length;
+
+            return srednia;
+        }
+       
+        double Wariancja (int [] tab, double srednia)
+        {
+            double suma = 0;
+            double wariancja = 0;
+            for (int i = 0; i < tab.Length; i++)
+            {
+                suma = suma + ((tab[i] - srednia)* (tab[i] - srednia));
+            }
+
+            wariancja = suma / tab.Length;
+
+            return wariancja;
+        }
+        
+        double Separacje (int[][] tab, int decyzja, int atrybut)
+        {
+            int[] wartObKlasy = WartosciZKlasy(tab, decyzja, atrybut);
+            int[] wartReszty = WartosciPozostale(tab, decyzja, atrybut);
+            int cardC = wartObKlasy.Length;
+            int cardU = daneZPliku.Length;
+
+            double cZKlasy = 0;
+            double cPozaKl = 0;
+            double zZKlasy = 0;
+            double zPozaKl = 0;
+            double separacja = 0;
+
+            cZKlasy = Srednia(wartObKlasy);
+            cPozaKl = Srednia(wartReszty);
+            zZKlasy = Wariancja(wartObKlasy, cZKlasy);
+            zPozaKl = Wariancja(wartReszty, cPozaKl);
+
+            separacja = Math.Pow((cZKlasy - cPozaKl), 2) / (zZKlasy + zPozaKl); 
+
+            return separacja;
+        }
+
+       
+        Dictionary<int, Dictionary<int, double>> NrAtrybutow(int[] tablicaZDecyzjami)
+        {
+            Dictionary<int, Dictionary<int, double>> NrAtrybutuIWartosc = new Dictionary<int, Dictionary<int, double>>();
+
+            List<int> listaDecyzji = new List<int>();
+            listaDecyzji = tablicaZDecyzjami.ToList();
+
+            int iloscAtrybutow = daneZPliku[0].Length - 1;
+
+            foreach (var decyzja in listaDecyzji) 
+            {
+                NrAtrybutuIWartosc.Add(decyzja, SeparacjaKlasyDecyzyjnej(decyzja));
+            }
+
+            return NrAtrybutuIWartosc;
+        }
+
+
+        Dictionary<int, double> SeparacjaKlasyDecyzyjnej(int decyzja)
+        {
+            Dictionary<int, double> slSeparacje = new Dictionary<int, double>();
+
+            int iloscAtrybutow = daneZPliku[0].Length - 1;
+
+            for (int i = 0; i < iloscAtrybutow; i++)
+            {
+                slSeparacje.Add(i, Separacje(daneZPliku, decyzja, i));
+            }
+
+            slSeparacje = slSeparacje.OrderByDescending(s => s.Value).ToDictionary(s => s.Key, s => s.Value);
+
+            return slSeparacje;
+        }
+
+        
+
+        List<int> NajlepszeSeparacje (Dictionary<int, Dictionary<int, double>> slownik)
+        {
+            List<int> lista = new List<int>();
+
+            int iloscPobrana = int.Parse(IloscAtrybutow.Text);
+
+            int licznikZnalezionych = 0;
+
+            while (licznikZnalezionych < iloscPobrana)
+            {
+                foreach (var decyzja in slownik)
+                {
+                    if (lista.Count < iloscPobrana)
+                    {
+                        if (!lista.Contains(decyzja.Value.First().Key))
+                        {
+                            lista.Add(decyzja.Value.First().Key);
+                            decyzja.Value.Remove(decyzja.Value.First().Key);
+                            licznikZnalezionych++;
+                        }
+
+                        else decyzja.Value.Remove(decyzja.Value.First().Key);
+                    }
+                }
+            }
+
+            return lista;
+        }
+
+    
+
+        string Wynik(List<int> listaZIndeksami)
+        {
+            string wynik = "";
+            List<int> lista = new List<int>();
+            lista = listaZIndeksami;
+
+            int iloscWierszy = daneZPliku.Length;
+
+            int indeksDecyzji = daneZPliku[0].Length - 1;
+            lista.Add(indeksDecyzji);
+
+            for (int i = 0; i < iloscWierszy; i++)
+            {
+                foreach (var indeks in lista)
+                {
+                    var result = string.Join(" ", daneZPliku[i][indeks]);
+                    wynik = wynik + " " + result.ToString();
+                }
+
+                wynik = wynik + "\r\n";
+            }
+
+            return wynik;
+        }
+
+
+        /*obsługa przycisków*/
+
+        private void WybierzSystem_Click(object sender, EventArgs e)
+        {
+            var wynik = ofd.ShowDialog();
+            if (wynik != DialogResult.OK)
+                return;
+
+
+
+            if (wynik == DialogResult.OK)
+            {
+
+                tbSciezka.Text = ofd.FileName;
+                string trescPliku = System.IO.File.ReadAllText(ofd.FileName);
+
+
+                string[] poziomy = trescPliku.Split('\n');
+
+                daneZPliku = new int[poziomy.Length][];
+
+                for (int i = 0; i < poziomy.Length; i++)
+                {
+                    string poziom = poziomy[i].Trim();
+                    string[] miejscaParkingowe = poziom.Split(' ');
+                    daneZPliku[i] = new int[miejscaParkingowe.Length];
+                    for (int j = 0; j < miejscaParkingowe.Length; j++)
+                    {
+                        daneZPliku[i][j] = int.Parse(miejscaParkingowe[j]);
+
+                    }
+                }
+
+
+                WczytanySystem.Text = trescPliku;
+                IloscAtrybutow.Maximum = (daneZPliku[0].Length - 1);
+                Generuj.Enabled = true;
+
+            }
+
+           
+         
+        }
+    
+        private void WczytanySystem_Load(object sender, EventArgs e)
+        {
+            ofd.Filter = "Text Filrd (.txt) |*.txt";
+            Generuj.Enabled = false;
+        }
+        
+
+        private void Generuj_Click(object sender, EventArgs e)
+        {
+            if (daneZPliku != null)
+            {
+                string wynik = "";
+
+                int[] decyzje = WartosciDecyzji();
+
+                Dictionary<int, Dictionary<int, double>> slownik = new Dictionary<int, Dictionary<int, double>>();
+                slownik = NrAtrybutow(decyzje);
+                List<int> listaAtrybutow = new List<int>();
+                listaAtrybutow = NajlepszeSeparacje(slownik);
+
+                wynik = Wynik(listaAtrybutow);
+
+                
+                KoncowaSelekcja.Text = wynik;
+            }
+            else 
+            {
+                Generuj.Enabled = false;
+
+            }
+
+           
+
+        }
+
+
+        private void IloscAtrybutow_ValueChanged(object sender, EventArgs e)
+        {
+
+            if(IloscAtrybutow.Value < 1)
+            {
+                KoncowaSelekcja.Text = "Wybierz liczbę atrubutów większą od 0!";
+                Generuj.Enabled = false;
+            }
+            else
+            {
+                KoncowaSelekcja.Text = "";
+                Generuj.Enabled = true;
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void KoncowaSelekcja_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void wyczysc_Click(object sender, EventArgs e)
+        {
+            WczytanySystem.Text = "";
+            KoncowaSelekcja.Text = "";
+            tbSciezka.Text = "";
+        }
+
+        private void zamknij_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
